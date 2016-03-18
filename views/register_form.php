d80af03df57162e1089da573646bd0414042df33
Juan Diego Martinez <juandimtzgza@gmail.com>
2016-03-18 15:04:41 +0000

    First and last name forms added

--- views/register_form.php
+++ views/register_form.php
@@ -1,13 +1,19 @@
 <form action="register.php" method="post">
     <fieldset>
         <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="firstname" placeholder="First Name" type="text"/>
+        </div>
+        <div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="lastname" placeholder="Last Name" type="text"/>
+        </div>
+        <div class="form-group">
             <input autocomplete="off" autofocus class="form-control" name="username" placeholder="Username" type="text"/>
         </div>
         <div class="form-group">
             <input class="form-control" name="password" placeholder="Password" type="password"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="confirmation" placeholder="Password" type="password"/>
+            <input class="form-control" name="confirmation" placeholder="Confirm Password" type="password"/>
         </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
