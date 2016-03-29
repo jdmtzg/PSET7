490426e0267f1c4b5b216869fe42d5718587e991
Juan Diego Martinez <juandimtzgza@gmail.com>
2016-03-29 13:58:08 +0000

    modification of background and format of buy

--- views/buy_form.php
+++ views/buy_form.php
@@ -1,14 +1,17 @@
+<body style="background-color: #ffcccc"/>
 <form action="buy.php" method="post">
     <fieldset>
         <div class="form-group">
-            <input class="form-control" name="symbol" placeholder="Symbol" type="text"/>
+            <input class="form-control" value="<?= $symbol_name ?>" name="symbol" placeholder="Symbol" type="text"/>
         </div>
         <div class="form-group">
             <input class="form-control" name="shares" placeholder="Num. of Shares" type="text"/>
         </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
-                Buy
+                <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>
+                    Buy
+                    
             </button>
         </div>
     </fieldset>
